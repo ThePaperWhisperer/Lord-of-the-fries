fb8f1530612d75fcb568bd2def3c73fdf1efd35d
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2025-04-10 16:58:27 -0400

    fix sky

--- app.js
+++ app.js
@@ -469,7 +469,7 @@ async function load() {
 	day.style.left = "50%";
 	document.body.appendChild(day);
 	setTimeout(async () => {
-		document.querySelector("a-sky").hidden = false
+		document.querySelector("#sky").hidden = false
 		for (var i = 0; i < 5; i++) {
 			wood = document.createElement("div");
 			wood.setAttribute("id", i);
